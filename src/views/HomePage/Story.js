bdade04ca9b74f11903535d72ae98dcbd2ee614b
AnhKhoa1408 <akhoa981@gmail.com>
2022-06-06 22:06:11 +0700

    feat: add utils class, Story section

---
+++ src/views/HomePage/Story.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Story() {
+  return (
+    <div className="container mw-12 story-container d-flex">
+      <div className="story-overview col-6">
+        <h4>OVERVIEW</h4>
+        <p>
+          In the 10 years, Clear Men has successfully educated & recruited 15%
+          Vietnamese Men, 25-45 to use Clear Men by:
+        </p>
+        <p>
+          <span>Functional offer:</span> Overcoming all the scalp’s challenges.
+        </p>
+        <p>
+          <span>Emotional offer:</span> Overcoming and beating no matter what
+          challenges the world throws at him.
+        </p>
+        <p>
+          We also engaged with Men by activating around their passion points,
+          from the king of sports - Football to the emerging ESport - Gaming.
+        </p>
+      </div>
+
+      <div className="story-image col-6">
+        <img
+          src="https://ufll2022-kova.vercel.app/_next/image?url=%2Fclear-men-bottle.png&w=1920&q=75"
+          alt=""
+        />
+        <img
+          src="https://ufll2022-kova.vercel.app/_next/image?url=%2Fclear-ronaldo.png&w=1920&q=75"
+          alt=""
+        />
+        <img
+          src="https://ufll2022-kova.vercel.app/_next/image?url=%2Fclear-unbeatable.png&w=1920&q=75"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Story;
