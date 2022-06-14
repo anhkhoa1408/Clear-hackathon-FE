f6fdd7c3061a48f9ac100ebaac59c3aabc85fea0
AnhKhoa1408 <akhoa981@gmail.com>
2022-06-14 17:29:22 +0700

    fix: fix scroll function

--- src/utils/scroll.js
+++ src/utils/scroll.js
@@ -1,33 +1,35 @@
 import { useEffect } from "react";
 
-function useAnimateScroll(id, offset = 0, reverse = false) {
+function useAnimateScroll(id, offset = 0, reverse = false, percentage = false) {
   useEffect(() => {
     let element = document.getElementById(id);
     let lastScrollTop = 0;
     let scrollListener = window.addEventListener("scroll", () => {
       let currentScroll = window.scrollY;
+      let scrollDiff = Math.abs(currentScroll - lastScrollTop);
+      scrollDiff = scrollDiff > 10 ? scrollDiff / 10 : scrollDiff;
       if (currentScroll > lastScrollTop) {
         if (element) {
           if (!reverse) {
-            element.style.transform = `translateY(calc(${
-              currentScroll / 100
-            }% - ${offset}px))`;
+            element.style.transform = `translateY(calc(${scrollDiff}% + ${offset}${
+              percentage ? "%" : "px"
+            }))`;
           } else {
-            element.style.transform = `translateY(calc(-${
-              currentScroll / 100
-            }% + ${offset}px))`;
+            element.style.transform = `translateY(calc(-${scrollDiff}% - ${offset}${
+              percentage ? "%" : "px"
+            }))`;
           }
         }
       } else {
         if (element) {
           if (!reverse) {
-            element.style.transform = `translateY(calc(-${
-              currentScroll / 100
-            }% + ${offset}px))`;
+            element.style.transform = `translateY(calc(-${scrollDiff}% - ${offset}${
+              percentage ? "%" : "px"
+            }))`;
           } else {
-            element.style.transform = `translateY(calc(${
-              currentScroll / 100
-            }% - ${offset}px))`;
+            element.style.transform = `translateY(calc(${scrollDiff}% + ${offset}${
+              percentage ? "%" : "px"
+            }))`;
           }
         }
       }
