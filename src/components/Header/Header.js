4406cd8d3e9bb8a946aa0803460dded8947928ea
AnhKhoa1408 <akhoa981@gmail.com>
2022-06-25 14:57:00 +0700

    feat: add scroll into view header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Header() {
+  const handleScroll = (id, e) => {
+    let element = document.getElementById(id)
+    if (element) {
+      element.scrollIntoView({behavior: "smooth"})
+      let navItem = document.getElementsByClassName("header--navlink")
+      Array.from(navItem).forEach(item => {
+        console.log(item.childNodes[0].classList)
+        item.childNodes[0].classList.remove("active")
+      })
+      e.target.classList.add("active")
+    }
 
+  }
 
   return (
     <div className="header">
@@ -10,16 +22,16 @@ function Header() {
         <img src="https://ufll2022-kova.vercel.app/clear-logo.svg" alt="" />
       </div>
       <div className="header--sidenav">
-        <div className="header--navlink">
-          <span>STORY</span>
+        <div onClick={(e) => handleScroll("story", e)} className="header--navlink">
+          <span className="active">STORY</span>
         </div>
-        <div className="header--navlink">
+        <div onClick={(e) => handleScroll("insight", e)} className="header--navlink">
           <span>INSIGHT</span>
         </div>
-        <div className="header--navlink">
+        <div onClick={(e) => handleScroll("big-idea", e)} className="header--navlink">
           <span>BIG IDEA</span>
         </div>
-        <div className="header--navlink">
+        <div onClick={(e) => handleScroll("deployment-plan", e)} className="header--navlink">
           <span>DEPLOYMENT PLAN</span>
         </div>
       </div>
