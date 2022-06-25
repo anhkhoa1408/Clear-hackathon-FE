fcbdd78f5a06faafdc6ce8b111288c8539852d4e
AnhKhoa1408 <akhoa981@gmail.com>
2022-06-25 21:15:05 +0700

    feat: active sidebar on scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,18 +2,63 @@ import React, { useEffect } from "react";
 
 function Header() {
   const handleScroll = (id, e) => {
-    let element = document.getElementById(id)
+    let element = document.getElementById(id);
     if (element) {
-      element.scrollIntoView({behavior: "smooth"})
-      let navItem = document.getElementsByClassName("header--navlink")
-      Array.from(navItem).forEach(item => {
-        console.log(item.childNodes[0].classList)
-        item.childNodes[0].classList.remove("active")
-      })
-      e.target.classList.add("active")
+      element.scrollIntoView({ behavior: "smooth" });
+      let navItem = document.getElementsByClassName("header--navlink");
+      Array.from(navItem).forEach((item) => {
+        item.childNodes[0].classList.remove("active");
+      });
+      e.target.classList.add("active");
+    }
+  };
+
+  useEffect(() => {
+    function activeNavOnScroll() {
+      let storyElement = document.getElementById("story");
+      let insightElement = document.getElementById("insight");
+      let ideaElement = document.getElementById("big-idea");
+      let planElement = document.getElementById("deployment-plan");
+      let navItem = document.getElementsByClassName("header--navlink");
+      Array.from(navItem).forEach((item) => {
+        item.childNodes[0].classList.remove("active");
+      });
+      let scrollOffset = window.scrollY + window.innerHeight;
+      if (
+        scrollOffset >= storyElement.offsetTop &&
+        scrollOffset <= storyElement.offsetTop + storyElement.offsetHeight
+      ) {
+        document
+          .getElementsByClassName("header--navlink")[0]
+          .childNodes[0].classList.add("active");
+      } else if (
+        scrollOffset >= insightElement.offsetTop &&
+        scrollOffset <= insightElement.offsetTop + insightElement.offsetHeight
+      ) {
+        document
+          .getElementsByClassName("header--navlink")[1]
+          .childNodes[0].classList.add("active");
+      } else if (
+        scrollOffset >= ideaElement.offsetTop &&
+        scrollOffset <= ideaElement.offsetTop + ideaElement.offsetHeight
+      ) {
+        document
+          .getElementsByClassName("header--navlink")[2]
+          .childNodes[0].classList.add("active");
+      } else if (
+        scrollOffset >= planElement.offsetTop &&
+        scrollOffset <= planElement.offsetTop + planElement.offsetHeight
+      ) {
+        document
+          .getElementsByClassName("header--navlink")[3]
+          .childNodes[0].classList.add("active");
+      }
     }
 
-  }
+    let scrollListener = window.addEventListener("scroll", activeNavOnScroll);
+
+    return () => window.removeEventListener(scrollListener);
+  }, []);
 
   return (
     <div className="header">
@@ -22,16 +67,28 @@ function Header() {
         <img src="https://ufll2022-kova.vercel.app/clear-logo.svg" alt="" />
       </div>
       <div className="header--sidenav">
-        <div onClick={(e) => handleScroll("story", e)} className="header--navlink">
+        <div
+          onClick={(e) => handleScroll("story", e)}
+          className="header--navlink"
+        >
           <span className="active">STORY</span>
         </div>
-        <div onClick={(e) => handleScroll("insight", e)} className="header--navlink">
+        <div
+          onClick={(e) => handleScroll("insight", e)}
+          className="header--navlink"
+        >
           <span>INSIGHT</span>
         </div>
-        <div onClick={(e) => handleScroll("big-idea", e)} className="header--navlink">
+        <div
+          onClick={(e) => handleScroll("big-idea", e)}
+          className="header--navlink"
+        >
           <span>BIG IDEA</span>
         </div>
-        <div onClick={(e) => handleScroll("deployment-plan", e)} className="header--navlink">
+        <div
+          onClick={(e) => handleScroll("deployment-plan", e)}
+          className="header--navlink"
+        >
           <span>DEPLOYMENT PLAN</span>
         </div>
       </div>
